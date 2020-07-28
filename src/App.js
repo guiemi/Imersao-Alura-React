7d180ed164a7db978ec91c97c84299acdc14cd4f
Guilherme <guiemi.teixeira@gmail.com>
2020-07-28 18:34:44 -0300

    Import all new components to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import Menu from './Components/Menu'
+import dadosIniciais from './data/dados_iniciais.json'
+import BannerMain from './Components/BannerMain'
+import Carousel from './Components/Carousel'
+import Footer from './Components/Footer'
+
 function App() {
   return (
     <div>
       <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-End? Trabalhando na área..."}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Footer/>
    </div>
+
   );
 }
 
