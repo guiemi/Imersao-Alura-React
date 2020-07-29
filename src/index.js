14566339ff5bb4ce04614d25feba7563d8fe035d
Guilherme <guiemi.teixeira@gmail.com>
2020-07-29 20:53:01 -0300

    Implementa as rotas e uma página de 404

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router
+} from 'react-router-dom';
+
+function CadastroVideo() {
+  return(
+    <div>
+      Página de cadastro de vídeo
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={ () => (<div>Erro 404 </div>) } />
+    </Switch>
+  </Router>,
+
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
