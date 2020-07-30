fcc57a838901b614c3f377c4a5228042440bf5ce
Guilherme <guiemi.teixeira@gmail.com>
2020-07-30 09:02:44 -0300

    Moves App and fixes imports

--- src/Pages/Home/App.js
+++ src/Pages/Home/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Menu from './Components/Menu'
-import dadosIniciais from './data/dados_iniciais.json'
-import BannerMain from './Components/BannerMain'
-import Carousel from './Components/Carousel'
-import Footer from './Components/Footer'
+import Menu from '../../Components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json'
+// import BannerMain from './Components/BannerMain'
+import BannerMain from '../../Components/BannerMain'
+import Carousel from '../../Components/Carousel'
+import Footer from '../../Components/Footer'
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import App from '../src/Pages/Home/App'
 import {
   Switch,
   Route,
