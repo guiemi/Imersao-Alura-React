f63b8708ff20beeaaf753aac799a5dd58c18dd89
Guilherme <guiemi.teixeira@gmail.com>
2020-08-07 11:42:26 -0300

    WIP: antes de transformar uma função anônima em uma função normal

--- src/Pages/Cadastro/Video/index.js
+++ src/Pages/Cadastro/Video/index.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../Components/PageDefault';
+import useForm from '../../../Hooks/useForm';
 import FormField from '../../../Components/FormField';
 import Button from '../../../Components/Button';
-import useForm from '../../../Hooks/useForm';
+import videosRepository from '../../../Repositories/videos';
 
 function CadastroVideo() {
   const history = useHistory();
+  const [categorias, setCategorias] = useState([]);
   const { handleChange, values } = useForm({
-
+    titulo: 'Vídeo Padrão',
+    url: 'https://www.youtube.com/watch?v=mRvBCXMSrBE',
+    categoria: 'Tech',
   });
+
+  useEffect(() => {
+    
+  })
   return (
     <PageDefault>
       <h1>Cadastro de Vídeo</h1>
 
       <form onSubmit={(event) => {
         event.preventDefault();
-        alert('Vídeo cadastrado com sucesso');
+        // alert('Vídeo cadastrado com sucesso!!!');
 
-        history.push('/');
+        videosRepository.create({
+          titulo: values.titulo,
+          url: values.url,
+          categoriaId: 1,
+        })
+          .then(() => {
+            console.log('Cadastrou com sucesso!');
+            history.push('/');
+          });
       }}
       >
         <FormField
